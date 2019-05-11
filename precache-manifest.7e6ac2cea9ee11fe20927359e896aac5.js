257e0b79fbd0db229d248beba4afc7bc8c65e5a4
anujsinghwd <anuj_singh@outlook.in>
2019-05-11 19:22:54 +0530

    Updates

---
+++ precache-manifest.7e6ac2cea9ee11fe20927359e896aac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31963bee664165cf23fa246f71bd0e8f",
+    "url": "/react-book-finder/index.html"
+  },
+  {
+    "revision": "73310d70e01c8939ce8b",
+    "url": "/react-book-finder/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "471faea73c7651ba1276",
+    "url": "/react-book-finder/static/js/2.25922ec4.chunk.js"
+  },
+  {
+    "revision": "73310d70e01c8939ce8b",
+    "url": "/react-book-finder/static/js/main.73d0656f.chunk.js"
+  },
+  {
+    "revision": "40410eed5cdf9799f06a",
+    "url": "/react-book-finder/static/js/runtime~main.ddf8d623.js"
+  }
+]);
